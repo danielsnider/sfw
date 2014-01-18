09dfc2e585c8ee91a4b16f02b59ce6bfb7a81c67
danielsnider <danielsnider12@gmail.com>
2014-01-17 20:42:26 -0500

    new route for search quary

--- myproject/views.py
+++ myproject/views.py
@@ -2,9 +2,21 @@ from pyramid.view import view_config
 
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
+def home_view(request):
 	#get size, and section from route
 	#request reddit JSON
 	#parse JSON for pictures with .jpg that are large enough
 	#send list of JSON objects which match the requirements to template for display
+	
+    print request
+
+    return {'project': 'MyProject'}
+
+
+@view_config(route_name='query', renderer='templates/mytemplate.pt')
+def query_view(request):
+    
+    print request.matchdict['subreddit']
+    print request.matchdict['minsize']
+
     return {'project': 'MyProject'}
